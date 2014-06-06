3030dae5fd9cd15349a78f8975909cedb4aabb58
ssl9ek <serena.lei.17@gmail.com>
2014-06-06 11:18:47 -0400

    post sms installation

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -34,13 +34,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.plugin.sms/www/sms.js",
+        "id": "org.apache.cordova.plugin.sms.Sms",
+        "clobbers": [
+            "window.sms"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.camera": "0.2.9",
-    "org.apache.cordova.device": "0.2.9"
+    "org.apache.cordova.device": "0.2.9",
+    "org.apache.cordova.plugin.sms": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
